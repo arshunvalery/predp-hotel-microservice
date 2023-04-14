0052e88814fc6b4c31cb3962562bc5f3f7c0ef5b
Art1111111sh <newarshunlife@mail.ru>
2023-04-14 18:07:11 +0300

    Fix minor issues

--- starter/src/main/java/com/mhotel/starter/Application.java
+++ starter/src/main/java/com/mhotel/starter/Application.java
@@ -3,13 +3,11 @@ package com.mhotel.starter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.mhotel")
 @EntityScan(basePackages = "com.mhotel.dao.model")
-//@ComponentScan("com.mhotel")
-@EnableJpaRepositories(basePackages = "com.mhotel")
+@EnableJpaRepositories(basePackages = "com.mhotel.dao")
 public class Application {
     public static void main(String[] args) {
 
